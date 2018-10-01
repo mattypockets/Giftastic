bf7892168ee95e516d54d27e29b7d295e6cb0303
Matt Gasper <mrgasper27@gmail.com>
2018-10-01 17:13:19 -0500

    Added JS for starting/stopping and adding new buttons. Changed theme to Pokemon.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,32 +1,97 @@
 $( document ).ready(function() {
 
-    var shows = ["My Hero Academia", "Kino's Journey", "Cells at Work", "Laid Back Camp"];
+    var pokemon = ["Bulbasaur", "Pikachu", "Eevee", "Haunter"];
 
+    
+    // Show buttons for each pokemon
     function renderButtons() {
 
-       // Clear all of the shows so that buttons don't repeat
+       // Clear all of the buttons so that buttons don't repeat
         $(".buttons").empty();
 
-        // Looping through the array of shows
-        for (var i = 0; i < shows.length; i++) {
+        // Looping through the array of pokemon
+        for (var i = 0; i < pokemon.length; i++) {
 
-          // Then dynamicaly generating buttons for each movie in the array
-          // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
           var a = $("<button>");
-          // Adding a class of show-btn to our button
-          a.addClass("show-btn");
-          // Adding a data-attribute
-          a.attr("data-name", shows[i]);
-          // Providing the initial button text
-          a.text(shows[i]);
-          // Adding the button to the buttons div
+
+          a.addClass("poke-btn");
+          a.attr("data-name", pokemon[i]);
+          a.text(pokemon[i]);
+
           $(".buttons").append(a);
         }
     }
 
-    
+    // Display gifs when button is clicked
+    $(document.body).on("click", "button", function() {
+        event.preventDefault();
+
+        var name = $(this).attr("data-name");
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+          name + "&api_key=GjAmS61CkZSQ8r9uxM5EY3G73hyeGOEM&limit=10";
+
+        $.ajax({
+          url: queryURL,
+          method: "GET"
+        })
+          .then(function(response) {
+            var results = response.data;
+            console.log(results);
+            for (var i = 0; i < results.length; i++) {
+              var gifDiv = $("<div>");
+              var rating = results[i].rating;
+              var p = $("<p>").text("Rating: " + rating);
+              var pokeImg = $("<img>");
+
+              pokeImg.attr("src", results[i].images.fixed_height_still.url);
+              pokeImg.attr("data-still", results[i].images.fixed_height_still.url);
+              pokeImg.attr("data-animate", results[i].images.fixed_height.url);
+              pokeImg.attr("data-state", "still");
+  
+              gifDiv.prepend(p);
+              gifDiv.prepend(pokeImg);
+              gifDiv.css("width", "250px");
+  
+              $(".gifSpace").prepend(gifDiv);
+            }
+          });
+      });
+
+      // Add button when a pokemon is searched
+
+      $(".addPokemon").on("click", function(event) {
+
+		event.preventDefault();
+
+		var character = $(".pokeSearch").val().trim();
+
+		pokemon.push(character);
+		$(".pokeSearch").val("");
+
+		renderButtons();
+
+	});
     
 
+
+      // Toggle between still and moving gifs on click
+      $(document.body).on("click", "img", function() {
+        
+          var state = $(this).attr('data-state');
+    
+          if (state === "still") {
+            $(this).attr("data-state", "animate");
+            $(this).attr('src', $(this).attr("data-animate"));
+          } else if (state === "animate") {
+            $(this).attr("data-state", "still");
+            $(this).attr('src', $(this).attr("data-still"));
+          }
+        });
+  
+
+
+
+    renderButtons();
 
 
 
